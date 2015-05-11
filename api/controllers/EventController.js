335eeac34d6e159e9397bae5ac9fac1b25981540
pataiadam <pataiadam@gmail.com>
2015-05-11 21:29:23 +0200

    Add event update function

--- api/controllers/EventController.js
+++ api/controllers/EventController.js
@@ -28,7 +28,35 @@ module.exports = {
     },
 
     update: function(req, res){
-
+        if(req.method==='GET'){
+            if(req.session.user===undefined && req.query.id===undefined){
+                return res.redirect('/login');
+            }
+            Event.findOne(req.query.id).exec(function(err, event){
+                if(err) {
+                    sails.log.debug(err);
+                    return res.redirect('/login');
+                }
+                return res.view(event);
+            });
+        }else{
+            sails.log.debug(req.session.user);
+            var up = {
+                email: req.body.email,
+                pushbullet: req.body.pushbullet,
+                name: req.session.user.name,
+                facebookId: req.session.user.facebookId,
+                id: req.session.user.id
+            };
+            User.update(req.session.user,up).exec(function(err, user){
+                if(err) {
+                    sails.log.debug(err);
+                    return res.redirect('/login');
+                }
+                req.session.user=user[0];
+                return res.redirect('/user/update');
+            });
+        }
     },
 
     destroy: function(req, res){
